4d87255469715d0a880c34b29a205bf591d1b789
qiankun <1975210542@qq.com>
2021-07-06 17:24:03 +0800

    vendor

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 1 5    10 15 20
+	//      6
+	s := []int{1, 5, 10, 15, 20}
+	fmt.Println(searchInsert(s, 5))
+}
+func searchInsert(nums []int, target int) int {
+	n := len(nums)
+	left, right := 0, n-1
+	ans := n
+	for left <= right {
+		mid := (right-left)>>1 + left
+		if target <= nums[mid] {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return ans
 }
